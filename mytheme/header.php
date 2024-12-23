a722b8a94fb8017871528ca7268adde6dcb875fd
Sleppy-witch <kate.shemetovich@gmail.com>
2024-12-23 23:28:38 +0300

    header.php

---
+++ mytheme/header.php
@@ -0,0 +1,9 @@
+<header>
+            <nav>
+                <ul>
+                    <li class="logo"><img src="http://cite/wordpress/wp-content/uploads/2024/12/iconca.png" height="46px" width="46px">Title crime</li>
+                    <li class="left"><a href="404.php"><i class="fa-solid fa-book-skull"></i> Категории</a></li>
+                    <li class="left"><a href="404.php">О сайте</a></li>
+
+            </nav>
+        </header>
