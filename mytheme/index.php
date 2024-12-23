aa7b39be03497e27230acff2ac037265b7b18ed3
Sleppy-witch <kate.shemetovich@gmail.com>
2024-12-23 23:26:45 +0300

    index.php

---
+++ mytheme/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Тру‑крайм истории о преступлениях">
+    <title>Title crime</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri("style.css"); ?>" />
+    <script src="https://kit.fontawesome.com/9f008e080e.js"></script>
+    <link type="image/x-icon" href="http://cite/wordpress/wp-content/uploads/2024/12/iconca.ico" rel="icon">
+  </head>
+  <body background="http://cite/wordpress/wp-content/uploads/2024/12/background.png">
+    <?php get_header(); ?>
+    <?php get_sidebar(); ?>
+  </body>
+</html>
